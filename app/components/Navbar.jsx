4e7a7f8ffa1d0255cc8cdb780223e502b23a05e1
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-08 00:49:29 +0300

    Update Navbar styles and button text
    Moved font-Ovo class from Home link to the menu list for consistent styling. Changed 'Contact' button text to 'Connect' for improved clarity.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -15,11 +15,9 @@ const Navbar = () => {
             </a>
 
             {/* menu */}
-            <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
+            <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 font-Ovo">
                <li>
-                  <a className="font-Ovo" href="#top">
-                     Home
-                  </a>
+                  <a href="#top">Home</a>
                </li>
                <li>
                   <a href="#about">About me</a>
@@ -41,7 +39,7 @@ const Navbar = () => {
                   href="#contact"
                   className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
                >
-                  Contact
+                  Connect
                   <Image src={assets.arrow_icon} alt="" className="w-3" />
                </a>
             </div>

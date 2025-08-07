4e3fe2cc5d1010b2885848ab53cc373083c96b1f
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-07 23:09:18 +0300

    Add styling to Contact button in Navbar
    Applied Tailwind CSS classes to the Contact button for improved layout and visibility on large screens.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -35,7 +35,10 @@ const Navbar = () => {
 
             {/* btns */}
             <div>
-               <a href="#contact">
+               <a
+                  href="#contact"
+                  className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+               >
                   Contact
                   <Image src={assets.arrow_icon} alt="" className="w-3" />
                </a>

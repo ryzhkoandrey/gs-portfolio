59bfb7a40e63acc340e83a2377c121b6c8d1cd91
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-08 22:40:12 +0300

    Enhance Navbar UI with background and buttons
    Added a fixed background image to the Navbar and improved the button section by introducing theme and menu buttons. Updated menu styling for better visual appearance.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,6 +4,11 @@ import { assets } from '@/assets/assets';
 const Navbar = () => {
    return (
       <>
+         {/* background */}
+         <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+            <Image src={assets.header_bg_color} alt="" className="w-full" />
+         </div>
+
          <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
             {/* logo */}
             <a href="#top">
@@ -15,7 +20,10 @@ const Navbar = () => {
             </a>
 
             {/* menu */}
-            <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 font-Ovo">
+            <ul
+               className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3
+               font-Ovo bg-white/50 shadow-sm"
+            >
                <li>
                   <a href="#top">Home</a>
                </li>
@@ -34,14 +42,26 @@ const Navbar = () => {
             </ul>
 
             {/* btns */}
-            <div>
+            <div className="flex items-center gap-4">
+               {/* theme btn */}
+               <button>
+                  <Image src={assets.moon_icon} alt="" className="w-6" />
+               </button>
+
+               {/* connect btn */}
                <a
                   href="#contact"
-                  className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+                  className="hidden lg:flex items-center gap-3 px-10 py-2.5 
+                  border border-gray-500 rounded-full ml-4"
                >
                   Connect
                   <Image src={assets.arrow_icon} alt="" className="w-3" />
                </a>
+
+               {/* menu btn */}
+               <button className="block md:hidden ml-3">
+                  <Image src={assets.menu_black} alt="" className="w-6" />
+               </button>
             </div>
          </nav>
       </>

9f29ed39fb5f292a57a1284c081f6160c99187f8
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-14 00:53:24 +0300

    Add user image section to About component
    Imported assets and Next.js Image component to display a user image in the About section. Added layout structure for image and future content using flexbox.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,8 +1,23 @@
+import { assets } from '@/assets/assets';
+import Image from 'next/image';
+
 const About = () => {
    return (
       <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
          <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
          <h2 className="text-center text-5xl font-Ovo">About me</h2>
+
+         <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
+            <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+               <Image
+                  src={assets.user_image}
+                  alt="User"
+                  className="w-full rounded-3xl"
+               />
+            </div>
+
+            <div className="flex-1"></div>
+         </div>
       </div>
    );
 };

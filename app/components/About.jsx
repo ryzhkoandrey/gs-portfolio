7c62716a5ed9a4d151ceb263463d870433f1a51c
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-14 16:14:06 +0300

    +

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,13 +1,16 @@
-import { assets, infoList } from '@/assets/assets';
+import { assets, infoList, toolsData } from '@/assets/assets';
 import Image from 'next/image';
 
 const About = () => {
    return (
       <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+         {/* heading */}
          <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
          <h2 className="text-center text-5xl font-Ovo">About me</h2>
 
-         <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
+         {/* content */}
+         <div className="flex w-full flex-col lg:flex-row items-center justify-center gap-20 my-20">
+            {/* photo */}
             <div className="w-64 sm:w-80 rounded-3xl max-w-none">
                <Image
                   src={assets.user_image}
@@ -16,7 +19,9 @@ const About = () => {
                />
             </div>
 
-            <div className="flex-1">
+            {/* info */}
+            <div className="flex-1 max-w-2xl">
+               {/* text */}
                <p className="mb-10 max-w-2xl font-Ovo">
                   I am an experienced Frontend Developer with over a decade of
                   professional expertise in the field. Throughout my career, I have
@@ -24,16 +29,31 @@ const About = () => {
                   contributing to their success and growth.
                </p>
 
-               <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+               {/* list */}
+               <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6">
                   {infoList.map(({ icon, iconDark, title, description }, index) => (
                      <li
-                        className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer
+                        className="border-[0.5px] border-gray-400 rounded-xl p-6
                         hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black"
                         key={index}
                      >
                         <Image src={icon} alt={title} className="w-7 mt-3" />
                         <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
                         <p className="text-gray-600 text-sm">{description}</p>
+                     </li>
+                  ))}
+               </ul>
+
+               {/* tools */}
+               <h4 className="my-6 text-gray-700 font-Ovo">Tools I use</h4>
+               <ul className="flex items-center gap-3 sm:gap-5">
+                  {toolsData.map((tool, index) => (
+                     <li
+                        className="flex items-center justify-center w-12 sm:w-14 aspect-square
+                        border border-gray-400 rounded-lg hover:-translate-y-1 duration-500"
+                        key={index}
+                     >
+                        <Image src={tool} alt="Tool" className="w-5 sm:w-7" />
                      </li>
                   ))}
                </ul>

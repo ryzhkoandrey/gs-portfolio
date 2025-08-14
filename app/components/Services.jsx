5a2bfcee1b776748e33c0fec6b36ccd99751341e
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-14 19:20:03 +0300

    Services

---
+++ app/components/Services.jsx
@@ -0,0 +1,11 @@
+const Services = () => {
+   return (
+      <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
+         {/* heading */}
+         <h4 className="text-center mb-2 text-lg font-Ovo">What I offers</h4>
+         <h2 className="text-center text-5xl font-Ovo">My services</h2>
+      </div>
+   );
+};
+
+export default Services;

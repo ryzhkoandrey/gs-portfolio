982d8c63a9305c6863c561fe18e3b6ec113cf29c
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-16 15:47:30 +0300

    contact

---
+++ app/components/Contact.jsx
@@ -0,0 +1,17 @@
+const Contact = () => {
+   return (
+      <div id="contact" className="w-full px-[12%] py-10 scroll-mt-20">
+         {/* heading */}
+         <h4 className="text-center mb-2 text-lg font-Ovo">Connect with me</h4>
+         <h2 className="text-center text-5xl font-Ovo">Get in touch</h2>
+
+         {/* text */}
+         <p className="text-center max-w-xl mx-auto mt-5 mb-12 font-Ovo">
+            I'd love to hear from you! If you have any questions, comments or
+            feedback, please use the form below.
+         </p>
+      </div>
+   );
+};
+
+export default Contact;

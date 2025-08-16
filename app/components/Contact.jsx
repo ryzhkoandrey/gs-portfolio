b8f2934f32f0e621be78c16163cfe15d9e5272cc
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-16 16:27:20 +0300

    contact

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,6 +1,10 @@
 const Contact = () => {
    return (
-      <div id="contact" className="w-full px-[12%] py-10 scroll-mt-20">
+      <div
+         id="contact"
+         className="w-full px-[12%] py-10 scroll-mt-20 bg-no-repeat bg-center bg-[length:90%_auto]"
+         style={{ backgroundImage: 'url("/footer-bg-color.png")' }}
+      >
          {/* heading */}
          <h4 className="text-center mb-2 text-lg font-Ovo">Connect with me</h4>
          <h2 className="text-center text-5xl font-Ovo">Get in touch</h2>
